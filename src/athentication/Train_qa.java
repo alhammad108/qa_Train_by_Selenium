41155457e8dcc09d5cafa70d7ca28136628c381e
alhammad108 <ahmadjawadalhammad@gmail.com>
2022-11-28 20:44:38 +0300

    Update

---
+++ src/athentication/Train_qa.java
@@ -0,0 +1,17 @@
+package athentication;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Train_qa {
+
+	public static void main(String[] args) {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\webdriver\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+	
+		
+
+	}
+
+}
